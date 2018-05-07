1748585a2bb921a20ff61475dbe817765ce2fa53
Riyad <rayabed@gmail.com>
2018-05-07 14:06:20 -0400

    First commit

---
+++ LabNumber10GC/Circle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber10GC
+{
+    class Circle
+    {
+        //data members
+        #region DataMembers
+        private double radius;
+        #endregion
+
+
+        //Properties
+        #region Properties
+        public double Radius
+        {
+            set
+            {
+                radius = value;
+            }
+            get
+            {
+                return radius;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public double CalculateCircumference()
+        {
+            
+            double circum = (2 * (Math.PI * Radius));
+            return circum;
+        }
+        public double CalculateArea()
+        {
+            double area = (Math.PI * (Radius * Radius));
+            return area;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            string formattedString = $"{CalculateCircumference(), 0:N2}";
+            return formattedString;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = $"{CalculateArea(),0:N2}";
+
+                return formattedArea;
+        }
+
+        #endregion
+    }
+
+
+
+
+
+}
---
+++ LabNumber10GC/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LabNumber10GC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle grandCircle = new Circle();
+            Console.WriteLine("Welcome to the Circle Tester");
+            int count = 0;
+            bool UserContinue = true;
+            while (UserContinue)
+            {
+                count++;
+                
+                //preferably some validation
+
+                grandCircle.Radius = IsDouble("Please enter the radius of the circle");
+                
+                
+                
+                Console.WriteLine("The circumference of the circle is: "
+                    + grandCircle.CalculateCircumference());
+                Console.WriteLine("The formatted cicumference of the circle is: " 
+                    + grandCircle.CalculateFormattedCircumference());
+                Console.WriteLine("The area of the circle is: "
+                    + grandCircle.CalculateArea());
+                Console.WriteLine("The formatted area of the circle is: " 
+                    + grandCircle.CalculateFormattedArea());
+                Console.WriteLine("Continue? y/n");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "n" || choice.ToLower() == "no")
+                {
+                    UserContinue = false;
+                    Console.WriteLine($"Goodbye. You created {count} Circle objects.");
+                }
+            }
+
+
+            
+        }
+
+        public static double IsDouble(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine();
+            while (!Regex.IsMatch(input, "^[0-9.]{1,}$"))
+            {
+                Console.WriteLine("Invalid input. Enter a valid number. " + prompt);
+                input = Console.ReadLine();
+            }
+                return double.Parse(input);
+            
+        }
+    }
+}
